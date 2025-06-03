c2b1759e8a37ed744ce46ecb1545bd7c12162acb
Mohamed <elsebaey95@outlook.com>
2025-06-04 01:30:25 +0300

    enhance images loading 10 by 10

--- app/our-projects/[projectName]/page.tsx
+++ app/our-projects/[projectName]/page.tsx
@@ -1,8 +1,7 @@
-// app/[projectName]/page.tsx
-import Image from "next/image";
 import { notFound } from "next/navigation";
-import { projects, ProjectImage } from "@/components/pages/our-projects/_constants";
+import { projects } from "@/components/pages/our-projects/_constants";
 import { Metadata } from "next";
+import ImageMasonryGallery from "@/components/sections/ImageMasonryGallery";
 
 type Props = {
   params: Promise<{
@@ -39,28 +38,7 @@ export default async function Page({ params }: Props) {
 
   return (
     <main className="min-h-screen content-width py-16">
-      <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4">
-        {project.images.map((img: ProjectImage, idx: number) => (
-          <div
-            key={idx}
-            className="relative mb-4 break-inside-avoid rounded-lg overflow-hidden shadow-lg"
-          >
-            <Image
-              src={img.url}
-              alt={img.alt || project.title}
-              width={img.width}
-              height={img.height}
-              sizes={`(max-width: 640px) 100vw,
-                      (max-width: 1024px) 50vw,
-                       33vw`}
-              className="w-full h-auto object-cover transition-transform duration-300 ease-in-out hover:scale-105"
-              placeholder={img.blurDataURL ? "blur" : undefined}
-              blurDataURL={img.blurDataURL}
-              loading="lazy"
-            />
-          </div>
-        ))}
-      </div>
+      <ImageMasonryGallery images={project.images} batchSize={10} />
     </main>
   );
 }
